b52bb490d219c51d3b964265e483301453755cb0
blackfoxgdl <ruben.alonso21@gmail.com>
2022-08-12 18:03:13 -0500

    new test

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json('Hello World');
 });
 
+app.get('/check', (req, res) => {
+  res.status(200).json({ name: 'Ruben' });
+});
+
 module.exports = app.listen(4000, () =>
   console.log(`Running on http://localhost:4000`)
 );
