2df92d4649926c2b7fafec34f0c56adf67ad846f
blackfoxgdl <ruben.alonso21@gmail.com>
2022-08-12 17:50:05 -0500

    new change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello World');
+  res.status(200).json({ tag: 'Hello World' });
 });
 
 module.exports = app.listen(4000, () =>
